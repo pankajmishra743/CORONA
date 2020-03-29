bb2bf44252b3219775cda0c003c0c3ad8d2b936d
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-03-29 19:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ state_df = state_df.replace(to_replace ='\(.*\)', value = '', regex = True)
 state_df.fillna(0, inplace=True)
 state_df = state_df.iloc[:, :-4]
 
-ColumnName = final_df['STATE_UT'].tolist()
+#ColumnName = final_df['STATE_UT'].tolist()
 #ColumnName.append('New')
-ColumnName.append(ColumnName.pop(ColumnName.index('All India')))
+#ColumnName.append(ColumnName.pop(ColumnName.index('All India')))
 #ColumnName.insert(0, "Date")
-state_df.columns = ColumnName
+#state_df.columns = ColumnName
 # state_df.rename(columns = {'Total':'All India'}, inplace = True)
 # state_df.rename(columns = {'Date (2020)':'Date'}, inplace = True)
 
