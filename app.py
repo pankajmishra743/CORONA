fc68ca9356c4c9f73b6fb8fcbb170095431d65ac
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-03-30 02:28:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import responses
 
 d = pd.read_html('https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India')
 df = d[7].iloc[:-2]
-df.columns = ['SN', 'STATE_UT', 'ACTIVE_CASES', 'DEATHS', 'RECOVERIES', 'TOTAL'] 
 df1 = df.iloc[:-1]
 df2 = df.tail(1)
 final_df = pd.concat([df2, df1]).reset_index(drop=True)
+final_df.columns = ['SN', 'STATE_UT', 'ACTIVE_CASES', 'DEATHS', 'RECOVERIES', 'TOTAL'] 
 final_df['STATE_UT'] = (final_df['STATE_UT'].str.strip(' †'))
-final_df.set_value(0, 'STATE_UT', 'All India') 
+final_df.set_value(0, 'STATE_UT', 'All India')
 
 dd = pd.read_html('https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India')
 state_df = dd[6].iloc[:-4]
