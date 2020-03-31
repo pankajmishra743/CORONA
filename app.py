cd2de2b9e4dbc7d7e77773361b0e039063d20178
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-03-31 15:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ final_df = pd.concat([df2, df1]).reset_index(drop=True)
 final_df.columns = ['SN', 'STATE_UT', 'ACTIVE_CASES', 'DEATHS', 'RECOVERIES', 'TOTAL'] 
 final_df['STATE_UT'] = (final_df['STATE_UT'].str.strip(' †'))
 final_df.set_value(0, 'STATE_UT', 'All India')
-final_df = final_df.replace(to_replace ='\(.*\)', value = '', regex = True)
 
 dd = pd.read_html('https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India')
 state_df = dd[6].iloc[:-4]
@@ -133,7 +132,7 @@ def sample_metadata(state):
     AllIndia_metadata = {}
     AllIndia_metadata['Deaths'] = ldf[ldf['STATE_UT']==state]['ACTIVE_CASES'].to_string(index=False)
     AllIndia_metadata['Recoveries'] = ldf[ldf['STATE_UT']==state]['DEATHS'].to_string(index=False)
-    AllIndia_metadata['TOTAL'] = ldf[ldf['STATE_UT']==state]['RECOVERIES'].to_string(index=False)
+    AllIndia_metadata['TOTAL'] = ldf[ldf['STATE_UT']==state]['RECOVERIES'].to_string(index=False).replace("#", "")
     a = pd.to_numeric(AllIndia_metadata['Deaths'], errors='coerce').astype(int)
     b = pd.to_numeric(AllIndia_metadata['Recoveries'], errors='coerce').astype(int)
     c = pd.to_numeric(AllIndia_metadata['TOTAL'], errors='coerce').astype(int)
