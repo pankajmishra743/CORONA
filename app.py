adb349ac77e478fad39d7b690a01edc9a8887238
pankajmishra743 <36882835+pankajmishra743@users.noreply.github.com>
2020-05-20 19:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import requests
 import responses
 
 d = pd.read_html('https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data/India_medical_cases')
-st_df = d[0].iloc[:-3]
-#st_df = df.iloc[:-1]
+df = d[0].iloc[:-3]
+st_df = df.iloc[:-1]
 #st_df.fillna(0, inplace=True)
 st_df.columns = ['SN', 'STATE_UT', 'TOTAL', 'DEATHS', 'RECOVERIES', 'ACTIVE_CASES'] 
 Total =  [pd.to_numeric(st_df.iloc[:, 2], errors='coerce').fillna(0).astype(int).sum(),
